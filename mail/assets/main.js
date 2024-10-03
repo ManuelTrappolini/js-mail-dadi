1735bb98d56a75bca4248801cb1d598ab17698f6
ManuelTrappolini <manuel.trappolini@gmail.com>
2024-10-03 16:41:28 +0200

    change for loop with .includes

--- mail/assets/main.js
+++ mail/assets/main.js
@@ -5,18 +5,31 @@ console.log('it works');
 
 //1. inizio definendo una variabile per la mail per il prompt ed un array sul quale controllare le diverse email
 //2 stabilisco condizione per cui cercare la mail nell'array con il ciclo for e voglio che il prompt inserendo la mail controlli che ci sia la mail nella lista
-const regMail = [ 'mario.rossi@gmail.com', 'paolo.verdi@gmail.com'  ]
+
+const regMail = [ 'mario.rossi@gmail.com', 'paolo.verdi@gmail.com','luca.bianchi@gmail.com', 'mimmo.forti@gmail.com'  ]
 let mail = prompt('Inserisci qui la tua mail')
+let message = 'Benvenuto'
+let negMessage = 'Reinserisci'
+/* for(let i = 0 ; i < regMail.length ; i++){
 
 
-for(let i = 0 ; i < regMail.length ; i++){
+
     if(regMail[i] === mail ){
-        console.log('Benvenuto');
+        console.log(message);
         alert('Benvenuto');
 }
-else {
-    console.log('Reinserisci');
+    else {
+    console.log(negMessage);
     alert('Reinserisci');
 }   
+} */
+
+    if(regMail.includes(mail) ){
+    console.log(message);
+    alert('Benvenuto nel sito!');
 }
 
+    else {
+    console.log(negMessage);
+    alert('Reinserisci la tua mail');
+}
