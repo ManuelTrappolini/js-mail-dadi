5ff9824cd378755a55fd3bf6e31b7515bda674f0
ManuelTrappolini <manuel.trappolini@gmail.com>
2024-10-03 15:52:15 +0200

    add for loop

--- mail/assets/main.js
+++ mail/assets/main.js
@@ -4,9 +4,17 @@ console.log('it works');
 //Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 
 //1. inizio definendo una variabile per la mail per il prompt ed un array sul quale controllare le diverse email
-const regMail = [ 'mario.rossi@gmail.com ', 'paolo.verdi@gmail.com', 'luca.bianchi@gmail.com'  ]
+//2 stabilisco condizione per cui cercare la mail nell'array con il ciclo for e voglio che il prompt inserendo la mail controlli che ci sia la mail nella lista
+const regMail = [ 'mario.rossi@gmail.com', 'paolo.verdi@gmail.com'  ]
 let mail = prompt('Inserisci qui la tua mail')
 
-//2 stabilisco condizione per cui cercare la mail nell'array con il metodo includes
 
-//2 voglio che il prompt inserendo la mail controlli che ci sia la mail nella lista
+for(let i = 0; i < regMail.length; i++){
+    if(regMail[i] === mail ){
+        console.log('Benvenuto')
+}
+else {
+    console.log('Reinserisci');
+}   
+}
+
